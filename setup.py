24c99b0d777930170df88096d2290a05ec13d910
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-06-13 00:14:19 +0200

    Upgraded zoos

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def get_all_files(path):
 
 setuptools.setup(
     name="lollms",
-    version="1.1.60",
+    version="1.1.62",
     author="Saifeddine ALOUI",
     author_email="aloui.saifeddine@gmail.com",
     description="A python library for AI personality definition",
