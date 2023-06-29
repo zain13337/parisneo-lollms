1a83053fbfdebdf29ee89a410fde12b16752050f
saloui <saifeddine.aloui@cea.fr>
2023-06-29 16:49:57 +0200

    added setuptools requirements

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def get_all_files(path):
 
 setuptools.setup(
     name="lollms",
-    version="2.0.31",
+    version="2.0.32",
     author="Saifeddine ALOUI",
     author_email="aloui.saifeddine@gmail.com",
     description="A python library for AI personality definition",
