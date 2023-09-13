8cd8dccf14f7cb6f81315c91b6c56fcc2ec60e3e
saloui <saifeddine.aloui@cea.fr>
2023-09-13 17:18:34 +0200

    fixed code

--- lollms/terminal.py
+++ lollms/terminal.py
@@ -387,20 +387,21 @@ class MainMenu(Menu):
         print(f"{ASCIIColors.color_green}Current binding: {ASCIIColors.color_reset}{self.lollms_app.config['binding_name']}")
         for p in self.lollms_app.lollms_paths.bindings_zoo_path.iterdir():
             if p.is_dir() and not p.stem.startswith("."):
-                with open(p/"binding_card.yaml", "r") as f:
-                    card = yaml.safe_load(f)
-                with open(p/"models.yaml", "r") as f:
-                    models = yaml.safe_load(f)
-                is_installed = (self.lollms_app.lollms_paths.personal_configuration_path/f"binding_{p.name}.yaml").exists()
-                entry=f"{ASCIIColors.color_green if is_installed else ''}{'*' if self.lollms_app.config['binding_name']==card['name'] else ''} {card['name']} (by {card['author']})"
-                bindings_list.append(entry)
-                entry={
-                    "name":p.name,
-                    "card":card,
-                    "models":models,
-                    "installed": is_installed
-                }
-                self.binding_infs.append(entry)
+                if (p/"binding_card.yaml").exists():
+                    with open(p/"binding_card.yaml", "r") as f:
+                        card = yaml.safe_load(f)
+                    with open(p/"models.yaml", "r") as f:
+                        models = yaml.safe_load(f)
+                    is_installed = (self.lollms_app.lollms_paths.personal_configuration_path/f"binding_{p.name}.yaml").exists()
+                    entry=f"{ASCIIColors.color_green if is_installed else ''}{'*' if self.lollms_app.config['binding_name']==card['name'] else ''} {card['name']} (by {card['author']})"
+                    bindings_list.append(entry)
+                    entry={
+                        "name":p.name,
+                        "card":card,
+                        "models":models,
+                        "installed": is_installed
+                    }
+                    self.binding_infs.append(entry)
         bindings_list += ["Back"]
         choice = self.show_menu(bindings_list)
         if 1 <= choice <= len(bindings_list)-1:
