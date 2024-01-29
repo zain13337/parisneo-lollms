3ecb4cfcf0d22eacef551d741ded6b7efcb57ff8
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-01-29 22:21:38 +0100

    fix

--- lollms/services/xtts/xtts_installer.bat
+++ lollms/services/xtts/xtts_installer.bat
@@ -2,20 +2,19 @@
 setlocal
 
 :: Check if portable Conda is installed
-where installer_files\miniconda3\Scripts\activate.bat >nul 2>&1
-if %errorlevel% == 0 (
+IF EXIST ".\installer_files\miniconda3\Scripts\activate.bat" (
     echo "Using portable Conda installation."
     set CONDA_BASE=installer_files\miniconda3
-    call "%CONDA_BASE%\Scripts\activate.bat"
-    call "%CONDA_BASE%\condabin\conda.bat" deactivate
-    call "%CONDA_BASE%\condabin\conda.bat" info --envs | findstr /B /C:"xtts" >nul 2>&1
+    call .\installer_files\miniconda3\Scripts\activate.bat
+    call .\installer_files\miniconda3\condabin\conda.bat deactivate
+    call .\installer_files\miniconda3\condabin\conda.bat info --envs | findstr /B /C:"xtts" >nul 2>&1
     if %errorlevel% == 0 (
         echo "Conda environment 'xtts' already exists. Deleting it."
-        call "%CONDA_BASE%\condabin\conda.bat" env remove --name xtts --yes
+        call .\installer_files\miniconda3\condabin\conda.bat env remove --name xtts --yes
     )
-    call "%CONDA_BASE%\condabin\conda.bat" create --name xtts --yes
-    call "%CONDA_BASE%\Scripts\activate.bat" xtts
-    call "%CONDA_BASE%\Scripts\pip.exe" install xtts-api-server --user
+    call .\installer_files\miniconda3\condabin\conda.bat create --name xtts --yes
+    call .\installer_files\miniconda3\Scripts\activate.bat xtts
+    call .\installer_files\miniconda3\Scripts\pip.exe install xtts-api-server --user
 ) else (
     echo "No portable Conda found. Checking for system-wide Conda installation."
     where conda >nul 2>&1
--- lollms/services/xtts/xtts_run.bat
+++ lollms/services/xtts/xtts_run.bat
@@ -1,14 +1,11 @@
 @echo off
 setlocal
-
+cd
 :: Check if portable Conda is installed
-where installer_files\miniconda3\Scripts\activate.bat >nul 2>&1
-if %errorlevel% == 0 (
+IF EXIST ".\installer_files\miniconda3\Scripts\activate.bat" (
     echo "Using portable Conda installation."
-    set CONDA_BASE=installer_files\miniconda3
-    call "%CONDA_BASE%\Scripts\activate.bat" xtts
-    call "%CONDA_BASE%\python.exe" -m xtts_api_server -o %1 -sf %2
-    call "%CONDA_BASE%\Scripts\deactivate.bat"
+    call .\installer_files\miniconda3\Scripts\activate.bat xtts
+    call .\installer_files\miniconda3\python.exe -m xtts_api_server -o %1 -sf %2
     exit /b
 ) else (
     echo "No portable Conda found. Checking for system-wide Conda installation."
