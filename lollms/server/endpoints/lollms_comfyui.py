7e37be141db8b479161af61ae2dec44bd45a6d81
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-03-19 22:14:57 +0100

    fix

--- lollms/server/endpoints/lollms_comfyui.py
+++ lollms/server/endpoints/lollms_comfyui.py
@@ -64,7 +64,7 @@ def start_comfyui():
         return {"status":True}
     except Exception as ex:
         lollmsElfServer.HideBlockingMessage()
-        lollmsElfServer.InfoMessage(f"It looks like I could not install SD because of this error:\n{ex}\nThis is commonly caused by a previous version that I couldn't delete. PLease remove {lollmsElfServer.lollms_paths.personal_path}/shared/comfyui manually then try again")
+        lollmsElfServer.InfoMessage(f"It looks like I could not install comfyui because of this error:\n{ex}\nThis is commonly caused by a previous version that I couldn't delete. PLease remove {lollmsElfServer.lollms_paths.personal_path}/shared/comfyui manually then try again")
         return {"status":False, 'error':str(ex)}
 
 @router.get("/show_comfyui")
